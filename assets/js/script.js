aee7f999abf88b0f123fbdd9795a4c27f2d70c8c
zdraste717 <evgeney2003@yandex.ru>
2024-03-26 04:12:52 +0300

    commit git -m "Flow"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,32 @@
 var swiper = new Swiper(".slide-content", {
     slidesPerView: 3,
-    slidesPerGroup: 3,
-    spaceBetween: 30,
+    spaceBetween: 40,
     loop: true,
-    loopFillGroupWithBlank: true,
     pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+      el: ".swiper-pagination",
+      clickable: true,
     },
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
-});
+    
+    breakpoints: {
+        1251: {
+            spaceBetween: 40,
+            slidesPerView: 3,
+        },
+
+        951: {
+            spaceBetween: 40,
+            slidesPerView: 2,
+        },
+
+        0: {
+            spaceBetween: 40,
+            slidesPerView: 1,
+        },
+    },
+  });
+
+  
